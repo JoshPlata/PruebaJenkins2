a5350feb74a15a87879feb6e667c6360e823fa92
JoshPlata <43298502+JoshPlata@users.noreply.github.com>
2020-09-11 14:49:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var webdriver = require('selenium-webdriver');
+var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'firefox' }).build();
+let gatos={};
+
+gatos.traergato=function(){
+    
+    browser.get('https://quebuscaras.herokuapp.com/inicio2.html').then(function(){
+        return browser.findElement(webdriver.By.id("breed_search"));
+    }).then(function(query){
+        return query.sendKeys("Abyssinian");
+    }).then(function(){
+        return browser.findElement(webdriver.By.id("botoncat"));
+    }).then(function(boton){
+        return boton.click();
+    }).catch((error) => {
+        console.log(error);
+    })
+
+}
+gatos.traergato();
+module.exports =gatos;
